3a434a8623b9ca2ec56f6097ebddfd153019ae7e
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-01-28 01:59:54 +0530

    modified fetching method to use versioning

--- static/js/background.js
+++ static/js/background.js
@@ -1,16 +1,31 @@
 // var test;
-$(document).ready(function(){
 
-    setInterval(function(){
-        // console.log("Hello");
-        $.getJSON("https://felicity.iiit.ac.in/api/schedule/", function(result){
-            $.each(result, function(version_id, schedule){
-                //$("div").append(field + " ");
-                console.log(version_id, schedule);
-                // test = schedule.events_data;
-                localStorage['schedule'] = JSON.stringify(schedule.events_data);
-            });
+$(document).ready(function(){
+    // localStorage['version_id'] = '';
+    function getEvents()
+    {
+        $.getJSON("https://felicity.iiit.ac.in/api/schedule/", {
+                prev_id : localStorage['version_id']
+            },
+            function(result){
+                console.log(result);
+                test = result;
+                if(!(result == undefined || result == ''))
+                {
+                    version_id = result.version_id;
+                    schedule = result.page_data;
+                    console.log(version_id, schedule);
+
+                    if(version_id)
+                    {
+                        localStorage['version_id'] = version_id;
+                        localStorage['schedule'] = JSON.stringify(schedule.events_data);
+                    }
+                }
         });
-    }, 5000);
-    //}, 3600000);
+
+    }
+    getEvents();
+    // setInterval(getEvents,5000);
+    setInterval(getEvents,3600000);
 });
