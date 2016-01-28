242330f52a5d6ef21f934f4f78ad533ee1caa0ef
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-01-29 00:56:13 +0530

    fetching events after every hour and for every popup

--- static/js/background.js
+++ static/js/background.js
@@ -1,30 +1,32 @@
 // var test;
 
-$(document).ready(function(){
+function getEvents()
+{
     // localStorage['version_id'] = '';
-    function getEvents()
-    {
-        $.getJSON("https://felicity.iiit.ac.in/api/schedule/", {
-                prev_id : localStorage['version_id']
-            },
-            function(result){
-                console.log(result);
-                test = result;
-                if(!(result == undefined || result == ''))
+    console.log("called");
+    $.getJSON("https://felicity.iiit.ac.in/api/schedule/", {
+            prev_id : localStorage['version_id']
+        },
+        function(result){
+            console.log(result);
+            test = result;
+            if(!(result == undefined || result == ''))
+            {
+                version_id = result.version_id;
+                schedule = result.page_data;
+                console.log(version_id, schedule);
+
+                if(version_id)
                 {
-                    version_id = result.version_id;
-                    schedule = result.page_data;
-                    console.log(version_id, schedule);
-
-                    if(version_id)
-                    {
-                        localStorage['version_id'] = version_id;
-                        localStorage['schedule'] = JSON.stringify(schedule.events_data);
-                    }
+                    localStorage['version_id'] = version_id;
+                    localStorage['schedule'] = JSON.stringify(schedule.events_data);
                 }
-        });
+            }
+    });
 
-    }
+}
+
+$(document).ready(function(){
     getEvents();
     // setInterval(getEvents,5000);
     setInterval(getEvents,3600000);
